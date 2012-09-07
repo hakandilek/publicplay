8cd4f0bc004aa81d0ed4b3361cfefc2e413da0e3
Hakan Dilek <hakandilek@gmail.com>
2012-09-07 16:58:30 +0200

    fix NPE

--- app/views/html/helper/H.java
+++ app/views/html/helper/H.java
@@ -4,7 +4,7 @@ package views.html.helper;
 public class H {
 
 	public static play.api.templates.Html nl2br(String s) {
-		return new play.api.templates.Html(s.replaceAll("\n", "<br/>"));
+		return new play.api.templates.Html(s != null ? s.replaceAll("\n", "<br/>") : "");
 	}
 	
 }
