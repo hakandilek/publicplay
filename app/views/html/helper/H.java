a105c826ccdc16c9c6fbd660c1613bca510eab18
Hakan Dilek <hakandilek@gmail.com>
2012-09-08 11:25:30 +0200

    enhance nl2br

--- app/views/html/helper/H.java
+++ app/views/html/helper/H.java
@@ -1,10 +1,17 @@
 package views.html.helper;
 
+import play.api.templates.Html;
+
 
 public class H {
 
-	public static play.api.templates.Html nl2br(String s) {
-		return new play.api.templates.Html(s != null ? s.replaceAll("\n", "<br/>") : "");
+	public static Html nl2br(String s) {
+		if (s == null)
+			return new Html("");
+		s = s.replaceAll("\n\r", "<br/>");
+		s = s.replaceAll("\r", "<br/>");
+		s = s.replaceAll("\n", "<br/>");
+		return new Html(s);
 	}
 	
 }
