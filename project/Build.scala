d33630288c9a9ba5d3cc41610ec1e5b9b730b5e9
Hakan Dilek <hakandilek@gmail.com>
2012-09-09 03:05:50 +0200

    fix dependency problem

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,18 @@ object ApplicationBuild extends Build {
     val appVersion      = "0.1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+        // Add your project dependencies here,
+        //add prettytime library
+        "org.ocpsoft.prettytime" % "prettytime" % "2.0.0-SNAPSHOT"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+        // Add your own project settings here
+        // The Typesafe repository
+        resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+
+		    // The Ocpsoft repository for PrettyTime
+        resolvers += "ocpsoft repository" at "http://ocpsoft.org/repository/"
     )
 
 }
