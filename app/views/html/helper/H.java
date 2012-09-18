e1f1c47893dc60c9b03f60d6b4461fd988a1bcb3
Hakan Dilek <hakandilek@gmail.com>
2012-09-18 23:28:32 +0200

    add some comments for the library

--- app/views/html/helper/H.java
+++ app/views/html/helper/H.java
@@ -7,12 +7,23 @@ import org.ocpsoft.prettytime.PrettyTime;
 
 import play.api.templates.Html;
 
-
+/**
+ * HTML Utils
+ * 
+ * @author hakan
+ */
 public class H {
 
-	//TODO: use request locale
+	// TODO: use request locale
 	private static PrettyTime prettyTime = new PrettyTime(Locale.ENGLISH);
-	
+
+	/**
+	 * replace NLs in text with HTML br tags
+	 * 
+	 * @param s
+	 *            text to replace
+	 * @return HTML output
+	 */
 	public static Html nl2br(String s) {
 		if (s == null)
 			return new Html("");
@@ -21,18 +32,39 @@ public class H {
 		s = s.replaceAll("\n", "<br/>");
 		return new Html(s);
 	}
-	
+
+	/**
+	 * textual representation of the given object
+	 * 
+	 * @param s
+	 *            object to display
+	 * @return HTML output
+	 */
 	public static Html string(Object s) {
 		return new Html("" + s);
 	}
-	
+
+	/**
+	 * prettifies the given date using Prettytime library
+	 * 
+	 * @param d
+	 *            date to show
+	 * @return HTML output
+	 */
 	public static Html prettify(Date d) {
 		if (d == null)
 			return new Html("");
 		String s = prettyTime.format(d);
 		return new Html(s);
 	}
-	
+
+	/**
+	 * Sanitizes and replaces the given text to be used in an URL
+	 * 
+	 * @param s
+	 *            text to replace
+	 * @return HTML output
+	 */
 	public static String sanitize(String s) {
 		if (s == null)
 			return "";
@@ -40,6 +72,5 @@ public class H {
 		s = s.toLowerCase(Locale.ENGLISH);
 		return s;
 	}
-	
-	
+
 }
