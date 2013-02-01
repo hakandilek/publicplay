5fb4aa14f03b64730e5ecd274cae103470c6373a
Hakan Dilek <hakandilek@gmail.com>
2013-02-01 21:43:45 +0100

    Convert Global.scala to java and do some error page prettiness for #76

---
+++ app/module/Global.java
@@ -0,0 +1,49 @@
+package module;
+
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+import play.mvc.Http.RequestHeader;
+import play.mvc.Result;
+
+import static play.mvc.Results.*;
+
+import views.html.errors.onNotFound;
+import views.html.errors.onError;
+import views.html.errors.onBadRequest;
+
+public class Global extends GlobalSettings {
+
+	private Application app;
+
+	public void onStart(Application app) {
+		Logger.info("application starting:"+app);
+		this.app = app;
+	}
+	
+	@Override
+	public Result onBadRequest(RequestHeader req, String error) {
+		if (app.isProd())  {
+			return badRequest(onBadRequest.render(req, error));
+		}
+		return super.onBadRequest(req, error);
+	}
+	
+	@Override
+	public Result onError(RequestHeader req, Throwable error) {
+		if (app.isProd())  {
+			return internalServerError(onError.render(error));
+		}
+		return super.onError(req, error);
+	}
+
+	@Override
+	public Result onHandlerNotFound(RequestHeader req) {
+		if (app.isProd())  {
+			return notFound(onNotFound.render(req));
+		}
+		return super.onHandlerNotFound(req);
+	}
+	
+	
+}
