cbeab062af783fbc0e1da3e91542cd4af9cf1930
Hakan Dilek <hakandilek@gmail.com>
2013-02-06 16:10:55 +0100

    minor code cleanup

--- app/controllers/crud/CategoryController.java
+++ app/controllers/crud/CategoryController.java
@@ -4,11 +4,8 @@ import javax.inject.Inject;
 
 import models.Category;
 import models.dao.CategoryDAO;
-import play.Logger;
 import play.mvc.Call;
-import play.mvc.Result;
 import play.utils.crud.CRUDController;
-import play.utils.crud.Parameters;
 import controllers.routes;
 
 public class CategoryController extends CRUDController<String, Category> {
@@ -38,21 +35,4 @@ public class CategoryController extends CRUDController<String, Category> {
 		return routes.Admin.categoryList();
 	}
 
-	@Override
-	public Result newForm() {
-		// TODO Auto-generated method stub
-		return super.newForm();
-	}
-
-	@Override
-	protected Result ok(String temp, Parameters params) {
-		if (Logger.isDebugEnabled())
-			Logger.debug("temp : " + temp);
-		if (Logger.isDebugEnabled())
-			Logger.debug("params : " + params);
-		// TODO Auto-generated method stub
-		return super.ok(temp, params);
-	}
-
-	
 }
