9818542ab37d68a7d21cbbe366de7d677e7992bb
Hakan Dilek <hakandilek@gmail.com>
2013-02-10 18:32:14 +0100

    S3File admin pages

--- app/models/S3File.java
+++ app/models/S3File.java
@@ -53,6 +53,10 @@ public class S3File extends Model implements TimestampModel<UUID> {
     @Transient
     public File file;
 
+	public String getBucket() {
+		return bucket;
+	}
+
 	@Override
 	public UUID getKey() {
 		return id;
